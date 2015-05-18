c743936163684c3f5757e5fb3bc759cd1ccd5d6f
Hayley Blackstock <hayleyblackstock@gmail.com>
2015-05-18 10:25:04 -0600

    created models for industries and companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ActiveRecord::Base
 
-  belongs_to :industry
-  
+  has_and_belongs_to_many :industry
+
 end
--- app/models/industry.rb
+++ app/models/industry.rb
@@ -1,5 +1,5 @@
 class Industry < ActiveRecord::Base
 
-  has_many :companies
-  
+  has_and_belongs_to_many :companies
+
 end
